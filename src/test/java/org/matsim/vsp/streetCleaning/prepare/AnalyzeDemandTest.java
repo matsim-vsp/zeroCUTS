b4aa784e3b730e7498e7728e64dc0013a16a4af5
rewert <ewert@vsp.tu-berlin.de>
2020-03-13 15:12:44 +0100

    add simple running test

---
+++ src/test/java/org/matsim/vsp/streetCleaning/prepare/AnalyzeDemandTest.java
@@ -0,0 +1,16 @@
+package org.matsim.vsp.streetCleaning.prepare;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnalyzeDemandTest {
+
+	@Test
+	public void runTest() {
+		try {
+			AnalyzeDemand.main(null);
+		} catch (Exception ee) {
+			Assert.fail("Not yet implemented");
+		}
+	}
+}
