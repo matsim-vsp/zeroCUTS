652d906ca3da176e7d35204816c6d8b08ab05a9c
Ricardo Ewert <ewert@vsp.tu-berlin.de>
2025-05-15 11:57:01 +0200

    update plots

--- src/main/R/Ricardo/AnalyzeCommercialModel.R
+++ src/main/R/Ricardo/AnalyzeCommercialModel.R
@@ -5,7 +5,7 @@ library(scales)
 
 
 # Datei einlesen – passe den Pfad ggf. an
-data <- read.csv2("C:/Users/erica/shared/matsim-metropole-ruhr/scenarios/metropole-ruhr-v2024.0/output/rvr/commercial_0.1pct/commercialTraffic_Run0pct/analysis/traffic/commercialTraffic_Run0pct.travelDistances_perVehicle.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8")
+data <- read.csv2("C:/Users/erica/shared/matsim-metropole-ruhr/scenarios/metropole-ruhr-v2024.0/output/rvr/commercial_100pct/commercialTraffic_Run100pct/analysis/traffic/commercialTraffic_Run100pct.travelDistances_perVehicle.csv", stringsAsFactors = FALSE, fileEncoding = "UTF-8")
 
 # Numerische Spalten umwandeln
 data$distanceInKm <- as.numeric(data$distanceInKm)
@@ -13,10 +13,32 @@ data$shareOfTravelDistanceWithDepotCharging <- as.numeric(data$shareOfTravelDist
 
 # Fahrzeugtypen zusammenfassen
 data$vehicleType <- ifelse(data$vehicleType %in% c("truck40t", "heavy40t"),
-                           "heavy40t", data$vehicleType)
+                           "40-Tonner", data$vehicleType)
+data$vehicleType <- ifelse(data$vehicleType %in% c("medium18t", "medium18t_parcel"),
+                           "18-Tonner", data$vehicleType)
 data$vehicleType <- ifelse(data$vehicleType %in% c("mercedes313", "mercedes313_parcel"),
-                           "mercedes313", data$vehicleType)
+                           "Sprinter", data$vehicleType)
+data$vehicleType <- ifelse(data$vehicleType %in% c("vwCaddy"),
+                           "Kleintransporter (z.B. VW Caddy)", data$vehicleType)
+data$vehicleType <- ifelse(data$vehicleType %in% c("golf1.4"),
+                           "PKW", data$vehicleType)
+data$vehicleType <- ifelse(data$vehicleType %in% c("light8t"),
+                           "8-Tonner", data$vehicleType)
+data$vehicleType <- ifelse(data$vehicleType %in% c("waste_collection_diesel"),
+                           "Abfallsammelfahrzeuge", data$vehicleType)
 data$vehicleType <- as.factor(data$vehicleType)
+
+data$subpopulation <- ifelse(data$subpopulation %in% c("commercialPersonTraffic", "commercialPersonTraffic_service"),
+                             "Personenwirtschaftsverkehr", data$subpopulation)
+data$subpopulation <- ifelse(data$subpopulation %in% c("FTL_kv_trip", "FTL_trip"),
+                                "FTL", data$subpopulation)
+data$subpopulation <- ifelse(data$subpopulation %in% c("goodsTraffic"),
+                                "Kleinräumiger Güterverkehr", data$subpopulation)
+data$subpopulation <- ifelse(data$subpopulation %in% c("LTL_trip"),
+                                "LTL", data$subpopulation)
+data$subpopulation <- ifelse(data$subpopulation %in% c("longDistanceFreight"),
+                                "Transit-Güterverkehr", data$subpopulation)
+data$subpopulation <- as.factor(data$subpopulation)
 
 # Berechnung für gesamte Flotte
 anteil_alle <- data %>%
@@ -26,7 +48,7 @@ anteil_alle <- data %>%
   mutate(label = paste0(round(anteil * 100, 1), "%"))
 
 
-# Anteil ≤ 100 % pro Fahrzeugtyp berechnen
+# Anteil ≤ 100 % pro Fahrzeugtyp berechnen (vehicleType)
 anteil_df <- data %>%
   group_by(vehicleType) %>%
   summarise(
@@ -35,7 +57,7 @@ anteil_df <- data %>%
   ) %>%
   mutate(label = paste0(round(anteil * 100, 1), "%"))
 
-# Anteil ≤ 80 % pro Fahrzeugtyp
+# Anteil ≤ 80 % pro Fahrzeugtyp (vehicleType)
 anteil80_df <- data %>%
   group_by(vehicleType) %>%
   summarise(
@@ -44,6 +66,25 @@ anteil80_df <- data %>%
   ) %>%
   mutate(label = paste0(round(anteil * 100, 1), "%"))
 
+# Anteil ≤ 100 % pro Fahrzeugtyp berechnen (subpopulations)
+anteil_df_subpopulation <- data %>%
+  group_by(subpopulation) %>%
+  summarise(
+    anteil = mean(shareOfTravelDistanceWithDepotCharging <= 1, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate(label = paste0(round(anteil * 100, 1), "%"))
+
+# Anteil ≤ 80 % pro Fahrzeugtyp (subpopulations)
+anteil80_df_subpopulation <- data %>%
+  group_by(subpopulation) %>%
+  summarise(
+    anteil = mean(shareOfTravelDistanceWithDepotCharging <= 0.8, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  mutate(label = paste0(round(anteil * 100, 1), "%"))
+
+
 # Anteil ≤ 80 % für gesamte Flotte
 anteil80_alle <- data %>%
   summarise(
@@ -51,12 +92,23 @@ anteil80_alle <- data %>%
   ) %>%
   mutate(label = paste0(round(anteil * 100, 1), "%"))
 
+# Hilfs-DataFrame für vertikale Linien
+linien_df <- data.frame(
+  xintercept = c(0.8, 1.0),
+  label = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen")
+)
 
 ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   geom_density(fill = "skyblue", alpha = 0.4) +
   geom_rug(sides = "b", alpha = 0.5) +
   # 100%-Strich + Text
   geom_vline(xintercept = 1.0, color = "red", linetype = "dashed", size = 1) +
+  geom_vline(data = linien_df, aes(xintercept = xintercept, color = label),
+             linetype = "dashed", size = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Grenzwerte",
+    values = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen" = "darkgreen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen" = "red")
+  ) +
   geom_text(
     data = anteil_alle,
     aes(x = 1.05, y = Inf, label = label),
@@ -75,19 +127,28 @@ ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   ) +
   scale_x_continuous(
     labels = scales::percent_format(accuracy = 1),
-    limits = c(0, NA)
+    limits = c(0, 7)
   ) +
   labs(
-    x = "Benötigte Reichweite im Verhältnis zur Reichweite des Elektrofahrzeugs ohne Nachladen",
-    title = "Dichteverteilung + Rugplot"
+    x = "Anteil der genutzten Reichweitenkapazität",
+    y = "Dichte",
+    title = "Fahrstrecke im Verhältnis zur Reichweite des Elektrofahrzeugs ohne Nachladen"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
 
 # Plot 2: Histogramm + Dichteverteilung
 ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   geom_histogram(aes(y = ..density..), bins = 75, fill = "lightblue", color = "white", alpha = 0.6) +
   geom_density(color = "darkblue", fill = "blue", alpha = 0.3) +
   geom_vline(xintercept = 1.0, color = "red", linetype = "dashed", size = 1) +
+  geom_vline(data = linien_df, aes(xintercept = xintercept, color = label),
+             linetype = "dashed", size = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Grenzwerte",
+    values = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen" = "darkgreen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen" = "red")
+  ) +
   geom_text(
     data = anteil_alle,
     aes(x = 1.1, y = Inf, label = label),
@@ -106,20 +167,27 @@ ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   ) +
   scale_x_continuous(
     labels = scales::percent_format(accuracy = 1),
-    limits = c(0, NA)
+    limits = c(0, 7)
   ) +
   labs(
-    x = "Anteil der Fahrstrecke mit Depotladung",
+    x = "Anteil der genutzten Reichweitenkapazität",
     y = "Dichte",
-    title = "Histogramm + Dichteverteilung"
+    title = "Fahrstrecke im Verhältnis zur Reichweite des Elektrofahrzeugs ohne Nachladen"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "bottom")
 
 # Plot 3: Verteilung der Depotladung nach Fahrzeugtyp
 ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   geom_histogram(aes(y = ..density..), bins = 30, fill = "lightblue", color = "white", alpha = 0.6) +
   geom_density(color = "darkblue", fill = "blue", alpha = 0.3) +
   geom_vline(xintercept = 1.0, color = "red", linetype = "dashed", size = 1) +
+  geom_vline(data = linien_df, aes(xintercept = xintercept, color = label),
+             linetype = "dashed", size = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Grenzwerte",
+    values = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen" = "darkgreen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen" = "red")
+  ) +
   # Annotation pro vehicleType (nach rechts versetzt)
   geom_text(
     data = anteil_df,
@@ -140,18 +208,66 @@ ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   facet_wrap(~ vehicleType, scales = "free_y") +
   scale_x_continuous(
     labels = scales::percent_format(accuracy = 1),
-    limits = c(0, NA)
+    limits = c(0, 7)
   ) +
   labs(
-    x = "Anteil der Fahrstrecke mit Depotladung",
+    x = "Anteil der genutzten Reichweitenkapazität",
     y = "Dichte",
-    title = "Verteilung der Depotladung nach Fahrzeugtyp"
+    title = "Fahrstrecke im Verhältnis zur Reichweite des Elektrofahrzeugs ohne Nachladen (Unterteilung nach Fahrzeugtyp)"
   ) +
   theme_minimal() +
   theme(
     plot.margin = margin(10, 40, 10, 10),
     clip = "off"
-  )
+  ) +
+  theme(legend.position = "bottom")
+
+# Plot 3-2: Verteilung der Depotladung nach Modell-Typ
+ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
+  geom_histogram(aes(y = ..density..), bins = 30, fill = "lightblue", color = "white", alpha = 0.6) +
+  geom_density(color = "darkblue", fill = "blue", alpha = 0.3) +
+  geom_vline(xintercept = 1.0, color = "red", linetype = "dashed", size = 1) +
+  geom_vline(data = linien_df, aes(xintercept = xintercept, color = label),
+             linetype = "dashed", size = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Grenzwerte",
+    values = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen" = "darkgreen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen" = "red")
+  ) +
+  # Annotation pro vehicleType (nach rechts versetzt)
+  geom_text(
+    data = anteil_df_subpopulation,
+    aes(x = 1.2, y = Inf, label = label),
+    inherit.aes = FALSE,
+    hjust = 0, vjust = 1.2,
+    color = "red", size = 3.5
+  ) +
+  # 80%-Strich + Text
+  geom_vline(xintercept = 0.8, color = "darkgreen", linetype = "dashed", size = 1) +
+  geom_text(
+    data = anteil80_df_subpopulation,
+    aes(x = 1.2, y = Inf, label = label),
+    inherit.aes = FALSE,
+    hjust = 0, vjust = 3.2,
+    color = "darkgreen", size = 3.5
+  ) +
+  facet_wrap(~ subpopulation, scales = "free_y") +
+  scale_x_continuous(
+    labels = scales::percent_format(accuracy = 1),
+    limits = c(0, 5)
+  ) +
+  labs(
+    x = "Anteil der genutzten Reichweitenkapazität",
+    y = "Dichte",
+    title = "Fahrstrecke im Verhältnis zur Reichweite des Elektrofahrzeugs ohne Nachladen (Unterteilung nach Modell-Typ)"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.margin = margin(10, 40, 10, 10),
+    clip = "off"
+  )  +
+  theme(legend.position = "bottom")
+
+
 
 # --- PLOT 4: Kumulative Häufigkeit (ECDF) nach vehicleType ---
 plot4 <- ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
@@ -159,6 +275,12 @@ plot4 <- ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
   facet_wrap(~ vehicleType, scales = "free_y") +
   geom_vline(xintercept = 1.0, color = "red", linetype = "dashed", size = 1) +
   geom_vline(xintercept = 0.8, color = "darkgreen", linetype = "dashed", size = 1) +
+  geom_vline(data = linien_df, aes(xintercept = xintercept, color = label),
+             linetype = "dashed", size = 1, show.legend = TRUE) +
+  scale_color_manual(
+    name = "Grenzwerte",
+    values = c("Anteil der Fahrzeuge, für die 80% der Batterie ausreichen" = "darkgreen", "Anteil der Fahrzeuge, für die 100% der Batterie ausreichen" = "red")
+  ) +
   # Text bei 80 %
   geom_text(data = anteil80_df,
             aes(x = 0.82, y = anteil, label = label),
@@ -171,12 +293,13 @@ plot4 <- ggplot(data, aes(x = shareOfTravelDistanceWithDepotCharging)) +
             inherit.aes = FALSE,
             hjust = 0, vjust = -0.5,
             color = "red", size = 3.5) +
-  scale_x_continuous(labels = percent_format(accuracy = 1), limits = c(0, 4)) +
+  scale_x_continuous(labels = percent_format(accuracy = 1), limits = c(0, 8)) +
   #scale_y_continuous(labels = percent_format(accuracy = 1)) +
   labs(
     x = "Depotladungsanteil",
     y = "Kumulative Verteilung"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "bottom")
 
 print(plot4)
